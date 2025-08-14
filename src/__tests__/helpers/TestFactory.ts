d4a766d293047a4fd9651914f77b57a4aee07493
Sumitgitup <sumitroy7august@gmail.com>
2025-08-14 11:02:45 +0530

    bank-logic changes

--- src/__tests__/helpers/TestFactory.ts
+++ src/__tests__/helpers/TestFactory.ts
@@ -1,6 +1,6 @@
 import User from '@/models/user';
 import Bank from '@/models/bank';
-import { BankAccountId, UserId } from '@/types/Common';
+import { BankAccountId, UserId } from '@/common/types';
 import GlobalRegistry from '@/services/GlobalRegistry';
 
 export interface TestFixtures {
